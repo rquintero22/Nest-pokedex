16ad395282452809bd41120fde9833e42c004b22
Raymond Quintero <quinteroaparicio@gmail.com>
2022-08-17 15:48:56 -0600

    Antes de patron adaptador

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -15,7 +16,8 @@ import { CommonModule } from './common/common.module';
       }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
-    CommonModule
+    CommonModule,
+    SeedModule
   ],
   controllers: [],
   providers: [],
